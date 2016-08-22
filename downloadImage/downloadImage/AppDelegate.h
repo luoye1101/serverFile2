8c19199f1e9bff9bdeb2bfb0e1b54e8cc967000c
huangyueqi <1611186361@qq.com>
2016-08-22 19:25:37 +0800

    创建项目

---
+++ downloadImage/downloadImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  downloadImage
+//
+//  Created by 黄跃奇 on 16/8/22.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ downloadImage/downloadImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  downloadImage
+//
+//  Created by 黄跃奇 on 16/8/22.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
